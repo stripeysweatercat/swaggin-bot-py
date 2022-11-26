ab6f65a79420cd50645b0a620f387f7152315484
PerigonSR <67944972+stripeysweatercat@users.noreply.github.com>
2022-11-26 17:57:42 +1100

    init

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+import config
+import asyncio
+import os
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is online')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await client.process_commands(message)
+
+    await message.channel.send
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+
+async def load():
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            await client.load_extension(f'cogs.{file[:-3]}')
+
+async def main():
+    await load()
+    await client.start(config.TOKEN)
+
+client.run(os.getenv('DISCORD_TOKEN'))
