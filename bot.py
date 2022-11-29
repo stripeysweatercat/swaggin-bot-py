b00a1652d2e3b875d01767088ce304c44d83647e
PerigonSR <67944972+stripeysweatercat@users.noreply.github.com>
2022-11-29 18:44:43 +1100

    background upload checking

--- bot.py
+++ bot.py
@@ -2,14 +2,18 @@ import discord
 import asyncio
 import os
 
+from itertools import cycle
 from discord.ext import commands
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
+status = cycle(['Status 1', 'Status 2', 'Status 3'])
 
 @client.event
 async def on_ready():
+    change_status.start()
     print(f'{client.user} is online')
 
 @client.event
@@ -28,6 +32,11 @@ async def load():
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
             await client.load_extension(f'cogs.{file[:-3]}')
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
 
 async def main():
     await load()
